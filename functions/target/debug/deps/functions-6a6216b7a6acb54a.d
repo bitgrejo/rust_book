1d9fd9b7f3352da82dc4e7f1bdcd69cdaeaa1327
bitgrejo <bitgrejo@protonmail.com>
2021-11-11 08:24:33 -0600

    Finished functions and comments chapters

---
+++ functions/target/debug/deps/functions-6a6216b7a6acb54a.d
@@ -0,0 +1,5 @@
+/Users/bitgrejo/projects/rust_book/functions/target/debug/deps/functions-6a6216b7a6acb54a: src/main.rs
+
+/Users/bitgrejo/projects/rust_book/functions/target/debug/deps/functions-6a6216b7a6acb54a.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/bitgrejo/projects/rust_book/functions/target/debug/functions: /Users/bitgrejo/projects/rust_book/functions/src/main.rs
