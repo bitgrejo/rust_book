70ac11632aa9720e1541b0e7721ca9d6bdcf4527
bitgrejo <bitgrejo@protonmail.com>
2021-11-11 08:47:14 -0600

    Finished Ch 3 by learning if/else and loops

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/bitgrejo/projects/rust_book/branches/target/debug/branches: /Users/bitgrejo/projects/rust_book/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-bb06fd7f61699332.d
@@ -0,0 +1,5 @@
+/Users/bitgrejo/projects/rust_book/branches/target/debug/deps/branches-bb06fd7f61699332: src/main.rs
+
+/Users/bitgrejo/projects/rust_book/branches/target/debug/deps/branches-bb06fd7f61699332.d: src/main.rs
+
+src/main.rs:
